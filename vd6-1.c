5484eb8839fdda7766bc224afe7da9ed297af748
TongMinhDuong <101384330+TongMinhDuong@users.noreply.github.com>
2022-03-17 01:26:17 +0700

    Add files via upload

---
+++ vd6-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+	int a = 10;
+	float b = 24.67892345;
+	char ch = 'A';
+	printf("\nInterger data = %d", a);
+	printf("\nFloat data = %f",b);
+	printf("\nCharacter = %c", ch);
+	printf("\nThis prints the string");
+	printf("\n%s","This also prints a string");
+}
